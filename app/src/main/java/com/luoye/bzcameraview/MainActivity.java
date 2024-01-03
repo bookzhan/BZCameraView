2f1b906c0357760e1d70c6f77fe659bbd7aa7a8b
bookzhan <bookzhan@qq.com>
2024-01-03 22:23:52 +0800

    版本更新

--- app/src/main/java/com/luoye/bzcameraview/MainActivity.java
+++ app/src/main/java/com/luoye/bzcameraview/MainActivity.java
@@ -22,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BZPermissionUtil.requestPermissionIfNot(this, Manifest.permission.CAMERA, BZPermissionUtil.CODE_REQ_PERMISSION);
+        BZPermissionUtil.requestCommonTestPermission(this);
     }
 
     public void Camera1Activity(View view) {
-        if (!BZPermissionUtil.requestPermissionIfNot(this, Manifest.permission.CAMERA, BZPermissionUtil.CODE_REQ_PERMISSION)) {
+        if (!BZPermissionUtil.isPermissionGranted(this, Manifest.permission.CAMERA)) {
             Toast.makeText(this, "Please give App sufficient permissions", Toast.LENGTH_LONG).show();
             return;
         }
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Camera2Activity(View view) {
-        if (!BZPermissionUtil.requestPermissionIfNot(this, Manifest.permission.CAMERA, BZPermissionUtil.CODE_REQ_PERMISSION)) {
+        if (!BZPermissionUtil.isPermissionGranted(this, Manifest.permission.CAMERA)) {
             Toast.makeText(this, "Please give App sufficient permissions", Toast.LENGTH_LONG).show();
             return;
         }
